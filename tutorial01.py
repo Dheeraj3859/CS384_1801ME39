7278bb57edecde517d0f4b2eddd2a14b15fe6c4b
manunem <manunem2000@gmail.com>
2020-09-16 18:13:10 +0530

    This is the first lab assignment

---
+++ tutorial01.py
@@ -0,0 +1,23 @@
+# Function to add two numbers 
+def add(num1, num2): 
+	addition = num1 + num2
+	return addition
+
+# Function to subtract two numbers 
+def subtract(num1, num2): 
+	subtraction = num1 - num2
+	return subtraction
+
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+	#Multiplication Logic 
+	return num1*num2
+
+# Function to divide two numbers 
+def divide(num1, num2): 
+	#DivisionLogic
+        if num2==0:
+            return 0
+        else:
+            return num1/num2
+	
