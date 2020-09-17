61d432f41a733bc083693d4341d195a9f9572676
manunem <manunem2000@gmail.com>
2020-09-17 12:33:21 +0530

    included some corner cases

--- tutorial01.py
+++ tutorial01.py
@@ -24,25 +24,41 @@ def divide(num1, num2):
 #You cant use the inbuilt python function x ** y . Write your own function
 def power(num1, num2): #num1 ^ num2
 	#DivisionLogic
+        flag=0
+        if num2<0:
+            flag=1
+            num2=num2*-1
+        num2 = int(num2)
+        if num1==0:
+            return 0
         ans=1
         for i in range(num2):
             ans = ans*num1
-        return ans
+        if flag==1:
+            return 1/ans
+        else:
+            return ans
 
 def printGP(a,r,n):
     gp=[]
+    if isinstance(n,float):
+        n = int(n)
     for i in range(n):
         gp.append(a*power(r,i))
     return gp
 
 def printAP(a,d,n):
     ap = []
+    if isinstance(n,float):
+        n = int(n)
     for i in range(n):
         ap.append(a+i*d)
     return ap
 
 def printHP(a,d,n):
     hp = []
+    if isinstance(n,float):
+        n = int(n)
     for i in range(n):
         den = a+i*d;
         if den==0:
@@ -50,3 +66,5 @@ def printHP(a,d,n):
         
         hp.append(round(1/den,3))
     return hp
+
+print(power(5,-2))
